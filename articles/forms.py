740768a11b2f18324db1cf695b86c11f8e289a51
ssmi8 <smithsteve8@hotmail.com>
2022-10-26 12:10:02 +0000

    Update: forms to allow user to post own articles

--- articles/forms.py
+++ articles/forms.py
@@ -1,9 +1,24 @@
-from .models import Comment
+from .models import Comment, Post
 from django import forms
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
 
 
 class CommentForm(forms.ModelForm):
     class Meta:
         model = Comment
         fields = ('body',)
-        
+
+
+class ArticleForm(forms.ModelForm):
+    class Meta:
+        model = Post
+        fields = ('title', 'content', 'excerpt', 'featured_image',)
+
+
+class UserRegisterForm(UserCreationForm):
+    email = forms.EmailField()
+
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'password1', 'password2']
