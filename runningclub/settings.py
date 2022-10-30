6da8198bdcf536c6c9423d7f61ee66761d66de38
ssmi8 <smithsteve8@hotmail.com>
2022-10-30 12:05:45 +0000

    favicon added and settings correction

--- runningclub/settings.py
+++ runningclub/settings.py
@@ -118,7 +118,7 @@ WSGI_APPLICATION = 'runningclub.wsgi.application'
 #         'ENGINE': 'django.db.backends.sqlite3',
 #         'NAME': BASE_DIR / 'db.sqlite3',
 #     }
-
+# }
 
 DATABASES = {
     'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
