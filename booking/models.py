3b864f3e94d5c79d9b19600b870c24e95f74ea84
ssmi8 <smithsteve8@hotmail.com>
2023-02-06 18:09:17 +0000

    Create booking app, and update model for runners at three levels to book a group run

---
+++ booking/models.py
@@ -0,0 +1,29 @@
+from django.db import models
+from django.contrib.auth.models import User
+from cloudinary.models import ClourdinaryField
+from django import forms
+
+
+# Create model for booking a running session
+
+
+class Booking(models.Model):
+    BEGINNERS = 'B'
+    INTERMEDIATE = 'I'
+    ADVANCED = 'A'
+    SESSIONS_CHOICES_CHOICES = [
+        (BEGINNERS, 'Running group for beginners level'),
+        (INTERMEDIATE, 'Running group for intermediate level'),
+        (ADVANCED, 'Running group for advanced runners'),
+    ]
+
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    booking_date_time = models.DateTimeField(auto_now=False, blank=True)
+
+    confirmed = models.BooleanField(default=False)
+
+
+class Home(models.Model):
+    title = models.CharField(max_length=200, unique=True)
+    slug = models.SlugField(max_length=200, unique=True)
