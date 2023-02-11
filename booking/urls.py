fa877a21e2b4b4d0e120a9cdeb894139611d8056
ssmi8 <smithsteve8@hotmail.com>
2023-02-11 11:04:37 +0000

    Update booking template vie and correct code in booking views.py to fix error

--- booking/urls.py
+++ booking/urls.py
@@ -2,7 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.booking, name='all_bookings'),
-    path('delete/<int:booking_id>', views.delete_booking, name="delete-booking"),
+    path('', views.booking, name='booking'),
+    path(
+        'delete/<int:booking_id>', views.delete_booking, name="delete-booking"
+        ),
     path('edit/<int:booking_id>', views.edit_booking, name='edit-booking'),
 ]
--- booking/views.py
+++ booking/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, HttpResponse, get_object_or_404, reverse, redirect
+from django.shortcuts import render, HttpResponse, get_object_or_404, reverse
 from django.views import generic, View
 from django.http import HttpResponse
 from .forms import BookingForm
@@ -15,27 +15,26 @@ def booking(request):
     if request.method == "GET":
         booking_form = BookingForm()
         existing_bookings = []
-        existing_bookings - Booking.objects.filter(
+        existing_bookings = Booking.objects.filter(
             confirmed=True, user=request.user
         ).all()
         print(existing_bookings)
-        return render(
-            request, 'booking.html', context={
-                "form": booking_form, "existing_bookings": existing_bookings})
+        return render(request, 'booking.html', context={
+            "form": booking_form, "existing_bookings": existing_bookings})
 
     elif request.method == "POST":
         booking_form = BookingForm(request.POST)
         if booking_form.is_valid():
             reservation = booking_form.save(commit=False)
-            reservation.user - request.user
+            reservation.user = request.user
             reservation.save()
             messages.add_message(
-                request, messages.SUCCESS, 'Booking successful')
-            return render(
-                request, 'booking_detail.html', {"booking": reservation})
+                request, messages.SUCCESS, 'Booking successful.')
+            return render(request, 'booking_detail.html', {
+                "booking": reservation})
         else:
             return render(request, 'booking.html', {"form": booking_form})
-        
+
 
 @login_required
 def bookings(request):
