a27a1315bc8e90d5e98aa45f12804b93bfe038bc
ssmi8 <smithsteve8@hotmail.com>
2023-02-11 16:09:09 +0000

    Update booking form CSS and update model with migration

--- booking/models.py
+++ booking/models.py
@@ -6,19 +6,25 @@ from django import forms
 
 # Create model for booking a running session
 
-BEGINNERS = 'B'
-INTERMEDIATE = 'I'
-ADVANCED = 'A'
-SESSIONS_CHOICES_CHOICES = [
-    (BEGINNERS, 'Running group for beginners level'),
-    (INTERMEDIATE, 'Running group for intermediate level'),
-    (ADVANCED, 'Running group for advanced runners'),
-]
-
-
 class Booking(models.Model):
+    BEGINNERS = 'B'
+    INTERMEDIATE = 'I'
+    ADVANCED = 'A'
+    SESSIONS_CHOICES_CHOICES = [
+        (BEGINNERS, 'Running group for beginners level'),
+        (INTERMEDIATE, 'Running group for intermediate level'),
+        (ADVANCED, 'Running group for advanced runners'),
+    ]
+    sessions_type = models.CharField(
+        max_length=1,
+        choices=SESSIONS_CHOICES_CHOICES,
+        default=BEGINNERS,
+    )
+
     user = models.ForeignKey(User, on_delete=models.CASCADE)
+
     booking_date_time = models.DateTimeField(auto_now=False, blank=True)
+
     confirmed = models.BooleanField(default=False)
 
 
