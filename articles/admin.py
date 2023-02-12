b6cdb9457e84c639269b269c76446483c52a59b6
ssmi8 <smithsteve8@hotmail.com>
2023-02-12 12:17:49 +0000

    remove approval for article posts, so they appear on article page

--- articles/admin.py
+++ articles/admin.py
@@ -6,15 +6,11 @@ from django_summernote.admin import SummernoteModelAdmin
 @admin.register(Post)
 class PostAdmin(SummernoteModelAdmin):
 
-    list_display = ('title', 'slug', 'status', 'created_on', 'approved')
+    list_display = ('title', 'slug', 'status', 'created_on')
     search_fields = ['title', 'content']
     prepopulated_fields = {'slug': ('title',)}
     list_filter = ('status', 'created_on')
     summernote_fields = ('content',)
-    actions = ['approve_posts']
-
-    def approve_posts(self, request, queryset):
-        queryset.update(approved=True)
 
 
 @admin.register(Comment)
--- articles/models.py
+++ articles/models.py
@@ -8,28 +8,30 @@ STATUS = ((0, "Draft"), (1, "Published"))
 class Post(models.Model):
     title = models.CharField(max_length=200, unique=True)
     slug = models.SlugField(max_length=200, unique=True)
-    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="article_posts")
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="article_posts")
     updated_on = models.DateTimeField(auto_now=True)
     content = models.TextField()
     featured_image = CloudinaryField('image', default='placeholder')
     excerpt = models.TextField(blank=True)
-    created_on = models.DateField(auto_now=True)
+    created_on = models.DateField(auto_now_add=True)
     status = models.IntegerField(choices=STATUS, default=0)
-    likes = models.ManyToManyField(User, related_name='articles_likes', blank=True)
-    approved = models.BooleanField('Approved', default=False)
+    likes = models.ManyToManyField(
+        User, related_name='articles_likes', blank=True)
 
     class Meta:
         ordering = ['-created_on']
 
     def __str__(self):
         return self.title
-            
+        
     def number_of_likes(self):
         return self.likes.count()
 
 
 class Comment(models.Model):
-    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name="comments")
+    post = models.ForeignKey(
+        Post, on_delete=models.CASCADE, related_name="comments")
     name = models.CharField(max_length=80)
     email = models.EmailField()
     body = models.TextField()
@@ -41,6 +43,3 @@ class Comment(models.Model):
 
     def __str__(self):
         return f"Comment {self.body} by {self.name}"
-    
-
-
